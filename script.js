7b52de0163a228bbee5ea5b4fc8777b6e4fefb2f
TekniqueJoseph <joejacksun@gmail.com>
2023-04-22 08:49:09 -0400

    added source code

---
+++ script.js
@@ -0,0 +1,12 @@
+let userInput = 'Hello';
+const isValidInput = userInput;
+
+
+// if(userInput){
+//     console.log('here')
+// } else {
+//     console.log('no here')
+// }
+
+const result = userInput ? true : false
+
