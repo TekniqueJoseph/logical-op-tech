271555fba0f50b6c4f7783dfb439bfe24f0027a8
TekniqueJoseph <joejacksun@gmail.com>
2023-04-22 09:33:01 -0400

    added bolean operators to assign values to variables

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 const userInput = '';
 
 
-const isValidInput = userInput;
+const isValidInput = !!userInput; //coverts the type
 // if(userInput){
 //     console.log('here')
 // } else {
 //     console.log('no here')
 // }
-
 // const result = userInput ? true : false
+const userName = userInput || 'Black';
 
+const rUserI = 'Shay';
+const rUserN = rUserI;
+
+const theUser = rUserI || 'Cam' 
