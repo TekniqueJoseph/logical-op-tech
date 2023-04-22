e7a65a3a90cfe648e63890e908b0355d36afc028
TekniqueJoseph <joejacksun@gmail.com>
2023-04-22 09:39:38 -0400

    added or's and and's tp assign shopping cart status

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
 const userInput = '';
-
-
-const isValidInput = !!userInput; //coverts the type
+const isValidInput = !!userInput; //coverts the type to real false
 // if(userInput){
 //     console.log('here')
 // } else {
 //     console.log('no here')
 // }
 // const result = userInput ? true : false
-const userName = userInput || 'Black';
+const userName = userInput || 'Black'; // user name = Black due to userInput being false
 
 const rUserI = 'Shay';
 const rUserN = rUserI;
+const theUser = rUserI || 'Cam' // theUser = Shat due to Shay being true
 
-const theUser = rUserI || 'Cam' 
+let isLoggedIn = true
+const Cart = isLoggedIn && ['Books']
+
+let stillIn = false
+const checkOut = stillIn && ['Books']
