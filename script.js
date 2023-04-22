967d47b0174732b0cf92fb42d2dff2280cd9d6d6
TekniqueJoseph <joejacksun@gmail.com>
2023-04-22 09:19:44 -0400

    added ternary operator for validation

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-let userInput = 'Hello';
+const userInput = '';
+
+
 const isValidInput = userInput;
-
-
 // if(userInput){
 //     console.log('here')
 // } else {
 //     console.log('no here')
 // }
 
-const result = userInput ? true : false
+// const result = userInput ? true : false
 
