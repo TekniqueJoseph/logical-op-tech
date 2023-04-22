2fb31dcc89ee27b2305a42dde4b0ca64b4364585
TekniqueJoseph <joejacksun@gmail.com>
2023-04-22 10:38:55 -0400

    added other test examples

--- script.js
+++ script.js
@@ -6,17 +6,20 @@ const isValidInput = !!userInput; //coverts the type to real false
 //     console.log('no here')
 // }
 // const result = userInput ? true : false
-const userName = userInput || 'Black'; // user name = Black due to userInput being false
+const userName = userInput || 0; // user name = Black due to userInput being false
 
 const rUserI = 'Shay';
 const rUserN = rUserI;
 const theUser = rUserI || 'Cam' // theUser = Shat due to Shay being true
 
-let isLoggedIn = true
-const Cart = isLoggedIn && ['Books']
+let check = null;
+const checkIt = check || '' // '' since both are false
 
-let stillIn = false
-const checkOut = stillIn && ['Books']
+let isLoggedIn = 'Joe'
+const Cart = isLoggedIn && ['Books'] // Books sice both are true
 
-let cashOut = true
-const chashier = cashOut && ''; 
+let stillIn = 'Joe'
+const checkOut = stillIn && '' // '' due to it being false
+
+let cashOut = null
+const chashier = cashOut && ''; // null since both are false
